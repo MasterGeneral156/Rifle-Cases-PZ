0f974fa0c08f06dde93ccb3a4c8ec4961343ecb6
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-12-25 19:58:51 -0500

    Initial release.

---
+++ media/lua/server/Items/Distributions_d165710d.lua
@@ -0,0 +1,259 @@
+Distributions = Distributions or {};
+
+local distributionTable = 
+{
+	bedroom = 
+	{
+		plankstash = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+		sidetable = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	shed = 
+	{
+		other = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+		logs = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	garagestorage = 
+	{
+		other = 
+		{
+			items = 
+			{
+			},
+		},
+		bin = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+	},
+	armystorage = 
+	{
+		locker = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+		metal_shelves = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	gunstore = 
+	{
+		counter = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",3,
+			},
+		},
+		displaycase = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",3,
+			},
+		},
+		locker = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",3,
+			},
+		},
+		metal_shelves = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	gunstorestorage = 
+	{
+		all = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",3,
+				"tmgRifleCases.Item_RifleCase",3,
+				"tmgRifleCases.Item_RifleCase",3,
+			},
+		},
+	},
+	armysurplus = 
+	{
+		shelves = 
+		{
+			items = 
+			{
+			},
+		},
+		metal_shelves = 
+		{
+			items = 
+			{
+			},
+		},
+		clothingrack = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+	},
+	armyhanger = 
+	{
+		metal_shelves = 
+		{
+			items = 
+			{
+			},
+		},
+		counter = 
+		{
+			items = 
+			{
+			},
+		},
+		locker = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+	},
+	hunting = 
+	{
+		locker = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",2,
+			},
+		},
+		metal_shelves = 
+		{
+			items = 
+			{
+			},
+		},
+		other = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	campingstorage = 
+	{
+		crate = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+	},
+	GunCache1 = 
+	{
+		GunBox = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+		counter = 
+		{
+			items = 
+			{
+			},
+		},
+		Bag_DuffelBagTINT = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	GunCache2 = 
+	{
+		GunBox = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+		Bag_DuffelBagTINT = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	SurvivorCache1 = 
+	{
+		medicine = 
+		{
+			items = 
+			{
+			},
+		},
+		SurvivorCrate = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",1,
+			},
+		},
+	},
+	SurvivorCache2 = 
+	{
+		medicine = 
+		{
+			items = 
+			{
+			},
+		},
+		SurvivorCrate = 
+		{
+			items = 
+			{
+				"tmgRifleCases.Item_RifleCase",2,
+			},
+		},
+	},
+}
+
+table.insert(Distributions, distributionTable);
